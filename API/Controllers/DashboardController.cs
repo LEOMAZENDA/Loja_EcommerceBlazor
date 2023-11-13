8965e799b5ad71045d081f9fce1b76ca097ce2fc
LEOMAZENDA <eng.leomazenda@gmail.com>
2023-11-13 21:39:14 +0100

    API de Ventas Feito

---
+++ API/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Contrato;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashdoardServico _servico;
+        public DashboardController(IDashdoardServico servico)
+        {
+            _servico = servico;
+        }
+
+
+    }
+}
---
+++ API/Controllers/VeltaController.cs
@@ -0,0 +1,37 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Contrato;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeltaController : ControllerBase
+    {
+        private readonly IVentaServico _servico;
+
+        public VeltaController(IVentaServico servico)
+        {
+            _servico = servico;
+        }
+
+
+        [HttpPost("Registar")]
+        public async Task<IActionResult> Registar([FromBody] VentaDTO dto)
+        {
+            var response = new ResponseDTO<VentaDTO>();
+            try
+            {
+                response.EstaCorreto = true;
+                response.Resultado = await _servico.Registar(dto);
+            }
+            catch (Exception ex)
+            {
+                response.EstaCorreto = true;
+                response.Mensagem = ex.Message;
+            }
+            return Ok(response);
+        }
+    }
+}
